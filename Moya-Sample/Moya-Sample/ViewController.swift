0c65b25dafa7c2db9dbee370af94bc2429d40f81
kentaro kimoto <exiler2411k@gmail.com>
2022-03-16 22:23:15 +0900

    init

---
+++ Moya-Sample/Moya-Sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Moya-Sample
+//
+//  Created by 木元健太郎 on 2022/03/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
