1aaf266b5d1d2df87747c806512119c8285d2661
kentaro kimoto <exiler2411k@gmail.com>
2022-03-20 16:52:49 +0900

    delete

--- Moya-Sample/Moya-Sample/Utility/GithubAPI.swift
+++ Moya-Sample/Moya-Sample/Utility/GithubAPI.swift
@@ -67,37 +67,3 @@ extension GithubAPI: TargetType {
         }
     }
 }
-
-
-
-
-//final class GithubAPI {
-//  static let shared = GithubAPI()
-//
-//  private init() {}
-//
-//  func get(searchWord: String, completion: ((Result<[GithubModel], GithubError>) -> Void)? = nil) {
-//    guard searchWord.count > 0 else {
-//      completion?(.failure(.error))
-//      return
-//    }
-//
-//
-//
-//    let url: URL = URL(string: "https://api.github.com/search/repositories?q=\(searchWord)&sort=stars")!
-//    let task: URLSessionTask = URLSession.shared.dataTask(with: url, completionHandler: {(data, response, error) in
-//
-//      guard let data = data,
-//            let githubResponse = try? JSONDecoder().decode(GithubResponse.self, from: data),
-//            let models = githubResponse.items else {
-//        completion?(.failure(.error))
-//        return
-//      }
-//      completion?(.success(models))
-//    })
-//    task.resume()
-//  }
-//}
-
-
-
